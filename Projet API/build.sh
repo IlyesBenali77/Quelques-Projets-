09a7b1674a7f25a0d79a185403aac59cb6273f01
Ilyes <benali_i@etna-alternance.net>
2023-12-08 18:57:21 +0100

    Mon Github

--- Projet API/build.sh
+++
@@ -1,5 +0,0 @@
-pip3 install -r requirements.txt --break-system-packages
-brew services start postgresql
-createdb protorh -U ilyes
-psql -d protorh -f database_rh.psql
-psql -U ilyes --password -d protorh
